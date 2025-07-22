d387a2626c769d9918795fd27ee319d639c8b986
Otaku-Essentials <essentials.otaku@gmail.com>
2025-07-22 22:50:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ Search.addEventListener("input",()=>
         if(text.includes(value))
         {
             match=1;
-            card.style.display="block";
+            card.style.display="flex";
         }
         else
         {
